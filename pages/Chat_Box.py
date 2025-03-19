704ad8ba2b9db8075c424faa8e996b89c16cbe55
ahpy-475 <haile05adam@gmail.com>
2025-03-19 11:58:33 -0400

    Add files via upload

---
+++ pages/Chat_Box.py
@@ -0,0 +1,2 @@
+import streamlit as st
+st.title("Hey this is the page where the chat box will be")
---
+++ pages/Web_API_Page.py
@@ -0,0 +1,2 @@
+import streamlit as st
+st.title("Hey this is the API Webpage where we are showing (insert name of the project that we're doing based off of the data here)")
