6b03e3a3868348cb92099e31833a5dcb62c4cd86
ahpy-475 <haile05adam@gmail.com>
2025-03-28 09:38:47 -0400

    Update and rename Chat_Box.py to Marty.py

--- pages/Chat_Box.py
+++
@@ -1,2 +0,0 @@
-import streamlit as st
-st.title("Hey this is the page where the chat box will be")
---
+++ pages/Marty.py
@@ -0,0 +1,2 @@
+import streamlit as st
+st.title("Hey this is the page where you will be able to talk with Marty!")
