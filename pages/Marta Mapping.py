b77e6effab1cdc6ae04317bf4cb8840721c95f09
aellis6 <anyaiellis6@gmail.com>
2025-04-03 17:42:35 -0400

    Update Marta Mapping.py

--- pages/Marta Mapping.py
+++ pages/Marta Mapping.py
@@ -31,23 +31,37 @@ if selected_station and selected_station != "Choose a station":
 
         for i, train in enumerate(trains_at_station):
             arrival_seconds = int(train.get("WAITING_SECONDS", "0"))
-            arrival_time = round(int(arrival_seconds) / 60)  
+
             
             with cols[i % 2]:  # Alternate between columns
+                if train.get("LINE") == "RED"
+                    box_color == #ff0000
+                if train.get("LINE") == "BLUE"
+                    box_color == #0000ff
+                if train.get("LINE") == "GREEN"
+                    box_color == #00ff00
+                if train.get("LINE") == "GOLD"
+                    box_color == #Ffd700
                 st.markdown(
                     f"""
-                    <div style="background-color:#1E88E5; color:white; padding:15px; border-radius:10px; text-align:center; margin-bottom:10px;">
+                    <div style="background-color:{box_color}; color:white; padding:15px; border-radius:10px; text-align:center; margin-bottom:10px;">
                         <h3>{train.get('LINE', 'Unknown')} Line</h3>
                         <p>🚊 <b>Arriving in {arrival_time} min</b></p>
                     </div>
                     """,
                     unsafe_allow_html=True
                 )
-            if arrival_time <1:
-                st.write(f"T-minus {arrival_seconds} seconds!")
-            elif arrival_time <5:
-                st.write(f"Almost here! The next train is arriving in about {arrival_time} minutes!")
-            else:
-                st.write(f"A little bit of a longer wait...The next train is arriving in about {arrival_time} minutes!")
+            
     else:
         st.warning("No on-time trains at this station.")
+
+
+def message():
+    if arrival_seconds <61:
+        st.write(f"T-minus {arrival_seconds} seconds!")
+    elif arrival_time <6000:
+        minutes = round(int(arrival_seconds) / 60)
+        st.write(f"Almost here! This train is arriving in about {minutes} minutes!")
+    else:
+        minutes = round(int(arrival_seconds) / 60)
+        st.write(f"A little bit of a longer wait...The next train is arriving in about {arrival_time} minutes!")
