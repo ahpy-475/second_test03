97e97fab16c5527d0db368b66ec916d717d09516
aellis6 <anyaiellis6@gmail.com>
2025-04-03 18:38:27 -0400

    Update Marta Mapping.py

--- pages/Marta Mapping.py
+++ pages/Marta Mapping.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 import pandas as pd
-import MARTA_DATA_SAMPLE.py as count
+import MARTA_DATA_SAMPLE as count
 st.title("MARTA Many & Much")
 #st.set_page_config(page_title="MARTA Many & Much", layout="wide")
 
