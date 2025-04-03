d69ec17fb0c89c291cae04d397632b12aef290da
aellis6 <anyaiellis6@gmail.com>
2025-04-03 16:41:20 -0400

    Update lab03info.py

--- lab03info.py
+++ lab03info.py
@@ -1,5 +1,5 @@
 ### INFO PAGE ###
 import streamlit as st
-adam_pf_pic = "pages/images/best pic (set1).jpg"
-anya_pf_pic = "pages/images/anya-headshot.jpg"
-gatech_pic = "pages/images/gatech.jpg"
+adam_pf_pic = "pages/Images/best pic (set1).jpg"
+anya_pf_pic = "pages/Images/anya-headshot.jpg"
+gatech_pic = "pages/Images/gatech.jpg"
