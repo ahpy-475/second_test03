62be7a8a3f6daa687035ee4671aee05c56ac1555
ahpy-475 <haile05adam@gmail.com>
2025-04-10 15:50:17 -0400

    Update Marty.py

--- pages/Marty.py
+++ pages/Marty.py
@@ -11,6 +11,10 @@ model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Write a poem about how learning web development is fun!")
 st.write(response.text)
 
-st.chat_input("Hey Transit Managers/Users! Ask Marty anything about MARTA")
+prompt = st.chat_input("Hey Transit Managers/Users! Ask Marty anything about MARTA")
+if prompt:
+  #code for answering a question
+  response = model.generate_content(prompt)
+  st.write(response.text)
 
-st.chat_message()
+#st.chat_message()
