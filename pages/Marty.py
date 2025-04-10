c75300f05f61df0f1826b028da5fcf3e6c42f72b
ahpy-475 <haile05adam@gmail.com>
2025-04-10 15:35:02 -0400

    Update Marty.py

--- pages/Marty.py
+++ pages/Marty.py
@@ -3,10 +3,10 @@ import os
 import google.generativeai as genai
 st.title("Chat with Marty!")
 
-
+genai.configure(apikey=st.secrets["key"])
 
 #client = genai.Client(api_key = key)
-model = genai.GenerativeModel(apikey = st.secrets["key"])
+model = genai.GenerativeModel("gemini-1.5-flash)
 response = model.generate_content("Write a poem about how learning web development is fun!")
 st.write(response.text)
 
