3e7f40b826411ac7defb3b4786c4c5b411d609a7
aellis6 <anyaiellis6@gmail.com>
2025-04-10 15:01:39 -0400

    Update Marty.py

--- pages/Marty.py
+++ pages/Marty.py
@@ -1,9 +1,15 @@
 import streamlit as st
 import os
 import google.generativeai as genai
-st.title("Hey this is the page where you will be able to talk with Marty!")
+st.title("Chat with Marty!")
+
+key1 = st.secrets[key]
 
 #client = genai.Client(api_key = key)
-model = genai.GenerativeModel(key)
+model = genai.GenerativeModel(key1)
 response = model.generate_content("Write a poem about how learning web development is fun!")
 st.write(response.text)
+
+st.chat_input("Hey Transit Managers/Users! Ask Marty anything about MARTA")
+
+st.chat_message()
