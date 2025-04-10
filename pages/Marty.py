9b179034ff2d16635d8698feba87f4764c67c890
ahpy-475 <haile05adam@gmail.com>
2025-04-10 15:51:44 -0400

    Update Marty.py

--- pages/Marty.py
+++ pages/Marty.py
@@ -8,8 +8,8 @@ genai.configure(api_key=key)
 
 #client = genai.Client(api_key = key)
 model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Write a poem about how learning web development is fun!")
-st.write(response.text)
+#response = model.generate_content("Write a poem about how learning web development is fun!")
+#st.write(response.text)
 
 prompt = st.chat_input("Hey Transit Managers/Users! Ask Marty anything about MARTA")
 if prompt:
