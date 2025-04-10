9f4976801515ed97ca342727db3b112621158e45
aellis6 <anyaiellis6@gmail.com>
2025-04-10 15:22:32 -0400

    Update About_Us.py

--- pages/About_Us.py
+++ pages/About_Us.py
@@ -17,7 +17,7 @@ if "shown" not in st.session_state:
 placeholder = st.empty()
 
 if not st.session_state.shown:
-    for i in range(62000000,65190801,100000):
+    for i in range(62000000,65190801,1):
         placeholder.metric("Rides in 2024", value=f"{i}")
         time.sleep(0.00000000000000000000000000000000000000000000000000000000000000000000000000000000001)
     st.session_state.shown = True
